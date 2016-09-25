9a78d0e416516d828fdfb2cb1201937bb78ae92a
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-09-25 06:10:05 +0100

    Set environment variables for spotlight
    The app is going to use these rather than getting its config from
    alphagov-deployment.

--- modules/govuk/manifests/apps/spotlight.pp
+++ modules/govuk/manifests/apps/spotlight.pp
@@ -26,6 +26,15 @@
 # [*nagios_memory_critical*]
 #   Memory use at which Nagios should generate a critical alert.
 #
+# [*backdrop_internal_url*]
+#   App config, internal hostname for backdrop
+#
+# [*backdrop_public_url*]
+#   App config, public hostname for backdrop
+#
+# [*stagecraft_url*]
+#   App config, hostname for stagecraft
+#
 class govuk::apps::spotlight (
   $alert_5xx_warning_rate,
   $alert_5xx_critical_rate,
@@ -34,10 +43,14 @@ class govuk::apps::spotlight (
   $publishing_api_bearer_token = undef,
   $nagios_memory_warning = undef,
   $nagios_memory_critical = undef,
+  $backdrop_internal_url = undef,
+  $backdrop_public_url = undef,
+  $stagecraft_url = undef,
 ) {
+  $app_name = 'spotlight'
 
   if $enabled {
-    govuk::app { 'spotlight':
+    govuk::app { $app_name:
       alert_5xx_warning_rate  => $alert_5xx_warning_rate,
       alert_5xx_critical_rate => $alert_5xx_critical_rate,
       app_type                => 'bare',
@@ -50,10 +63,23 @@ class govuk::apps::spotlight (
       nagios_memory_critical  => $nagios_memory_critical,
     }
 
-    govuk::app::envvar { "${title}-PUBLISHING_API_BEARER_TOKEN":
-      app     => 'spotlight',
-      varname => 'PUBLISHING_API_BEARER_TOKEN',
-      value   => $publishing_api_bearer_token,
+    Govuk::App::Envvar {
+      app => $app_name,
+    }
+
+    govuk::app::envvar {
+      "${title}-PUBLISHING_API_BEARER_TOKEN":
+        varname => 'PUBLISHING_API_BEARER_TOKEN',
+        value   => $publishing_api_bearer_token;
+      "${title}-BACKDROP_INTERNAL_URL":
+        varname => 'BACKDROP_INTERNAL_URL',
+        value   => $backdrop_internal_url;
+      "${title}-BACKDROP_PUBLIC_URL":
+        varname => 'BACKDROP_PUBLIC_URL',
+        value   => $backdrop_public_url;
+      "${title}-STAGECRAFT_URL":
+        varname => 'STAGECRAFT_URL',
+        value   => $stagecraft_url;
     }
   }
 }
