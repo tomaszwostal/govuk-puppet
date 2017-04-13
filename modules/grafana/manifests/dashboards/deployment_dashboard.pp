3d3087d16186d17bb8c9e38ed8eb1e8dc91986dc
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2017-04-13 15:52:51 +0100

    WIP: Start making bottom graphs conditional

--- modules/grafana/manifests/dashboards/deployment_dashboard.pp
+++ modules/grafana/manifests/dashboards/deployment_dashboard.pp
@@ -21,17 +21,30 @@
 # [*has_workers*]
 #   Whether the application uses workers
 #
+# [*has_controller_data*]
+#   Whether the application has Kibana data which is broken down by Rails
+#   controller
+#
 define grafana::dashboards::deployment_dashboard (
   $app_name = $title,
   $docs_name = $title,
   $dashboard_directory = undef,
   $app_domain = undef,
   $has_workers = false,
+  $has_controller_data = false,
 ) {
   if $has_workers {
     $worker_row = [['worker_failures', 'worker_successes']]
   } else {
     $worker_row = []
+  }
+
+  if $has_controller_data {
+    $errors_by_controller_row = [['errors_by_controller_action']]
+    $response_times_by_controller_row = [['response_times_by_controller']]
+  } else {
+    $errors_by_controller_row = []
+    $response_times_by_controller_row = []
   }
 
   $panel_partials = concat(
@@ -40,10 +53,8 @@ define grafana::dashboards::deployment_dashboard (
       ['error_deploy_counts', 'recent_500_count', 'period_500_count', 'links']
     ],
     $worker_row,
-    [
-      ['errors_by_controller_action'],
-      ['response_times_by_controller']
-    ]
+    $errors_by_controller_row,
+    $response_times_by_controller_row
   )
 
   file {
