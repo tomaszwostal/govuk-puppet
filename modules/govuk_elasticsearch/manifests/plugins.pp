47870f5f786c290f7466296d346ec6e3b3bd9b7f
Stephen Harker <stephen.harker@digital.cabinet-office.gov.uk>
2017-07-25 15:51:58 +0100

    Change to ES plugin class to not install cloud-aws if version > 1

--- modules/govuk_elasticsearch/manifests/plugins.pp
+++ modules/govuk_elasticsearch/manifests/plugins.pp
@@ -15,16 +15,17 @@ class govuk_elasticsearch::plugins {
     instances  => $::fqdn,
   }
 
-  case $govuk_elasticsearch::version {
-    /^1.4/:  { $cloud_aws_version = '2.4.2' }
-    /^1.5/:  { $cloud_aws_version = '2.5.1' }
-    /^1.7/:  { $cloud_aws_version = '2.7.1' }
-    default: { fail('Not able to select a version for cloud-aws, see https://github.com/elastic/elasticsearch-cloud-aws') }
-  }
+  if $govuk_elasticsearch::version =~ /^1\./ {
+    case $govuk_elasticsearch::version {
+      /^1.4/:  { $cloud_aws_version = '2.4.2' }
+      /^1.5/:  { $cloud_aws_version = '2.5.1' }
+      /^1.7/:  { $cloud_aws_version = '2.7.1' }
+      default: { fail('Not able to select a version for cloud-aws, see https://github.com/elastic/elasticsearch-cloud-aws') }
+    }
 
-  elasticsearch::plugin { "elasticsearch/elasticsearch-cloud-aws/${cloud_aws_version}":
-    module_dir => 'cloud-aws',
-    instances  => $::fqdn,
+    elasticsearch::plugin { "elasticsearch/elasticsearch-cloud-aws/${cloud_aws_version}":
+      module_dir => 'cloud-aws',
+      instances  => $::fqdn,
+    }
   }
-
 }
